9c2404b4883d3b67f6f216607c75f32f1101eeea
Walidbelhadj <47835010+Walidbelhadj@users.noreply.github.com>
2022-04-13 10:50:46 +0100

    initt

--- Pi/src/main/java/tn/esprit/spring/repository/CategorieEventRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.esprit.spring.entity.CategorieEvent;
-
-public interface CategorieEventRepository extends JpaRepository<CategorieEvent, Long>{
-
-}
